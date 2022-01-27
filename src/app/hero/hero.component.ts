7de572d91db68aff8f31464872b6edcaa9c9307f
shubhamyadav2000 <shubham.yadav@kuchanaya.com>
2022-01-27 22:56:56 +0530

    adding coousal margin

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -12,16 +12,13 @@ export class HeroComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
-
-
-
   images = [
-    { path: 'assets/george-cerny-YfTxKql9vDU-unsplash.jpg' },
-    { path: 'assets/laurent-jollet-k9Dw_K1ZIwA-unsplash.jpg' },
+    {path: 'assets/download.jpg'},
     { path: 'assets/paul-vhZe9fd9MRs-unsplash.jpg' },
+    { path: 'assets/laurent-jollet-k9Dw_K1ZIwA-unsplash.jpg' },
     { path: 'assets/silver-ringvee-Q6F08X4Okd8-unsplash.jpg' },
-    {path: 'assets/download.jpg'}
+    { path: 'assets/george-cerny-YfTxKql9vDU-unsplash.jpg' },
+  
   ];
 
 }
