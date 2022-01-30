eec6b3b3f70ce27afa598190f4645dc8c86534d3
shubhamyadav2000 <shubham.yadav@kuchanaya.com>
2022-01-30 20:50:58 +0530

    adding my server endpoints

--- src/app/common-service.service.ts
+++ src/app/common-service.service.ts
@@ -15,21 +15,10 @@ export class CommonServiceService {
 
 
   public getPNRStatus(pnr: any):Observable<any> {
-    // const pnrDetails={
-    //   'x-rapidapi-key':'646210614dmshfa1853333c3dfe0p12d7edjsn8392f50b07df',
-    //   'x-rapidapi-host':'pnr-status-indian-railway.p.rapidapi.com',
-    //   'Access-Control-Allow-Origin':'*',
-    //  }
-  
-    //  const httpOptions = {
-    //   headers: new HttpHeaders(pnrDetails)
-    //  }
-  //   let url = `https://pnr-status-indian-railway.p.rapidapi.com/rail/${pnr}`;
-  //   return this.http.get(url,httpOptions);
-  // }
-
     let url="http://travelhubserver.herokuapp.com/pnrStatus";
-    return this.http.post(url,pnr);
+    return this.http.post(url,{
+      "PNR_Number":pnr
+  });
 }
 
 }
